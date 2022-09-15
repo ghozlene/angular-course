40c1fc3a64e517eed52ad8161bd8dac15e327dae
Achref <medachrefg@gmail.com>
2022-09-15 11:48:10 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { GrandChildComponent } from './grand-child/grand-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildAComponent,
+    ChildBComponent,
+    GrandChildComponent
   ],
   imports: [
     BrowserModule
